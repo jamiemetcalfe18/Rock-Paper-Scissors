6aa245a7fb4d79fef1ca521d7faf8baabb62f4f5
Jamie Metcalfe <jamie.jet18@gmail.com>
2022-01-03 04:28:31 +0000

    Decision tree updated and assigned vars and determinations

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-
-
-prompt("Please choose Rock, Paper, or Scissors!");
-
 /* game logic function */
-function computerPlay()
-{
-  return Math.floor(Math.random() * 3);
+function computerPlay() {
+  let comptuerChoice = Math.floor(Math.random() * 3);
 
-   if (prob == 0)
+   if (computerChoice == 0)
      {
       return "rock";
      }
-   else if (prob == 1)
+   else if (computerChoice == 1)
      {
       return "paper";
      }
-
+   else 
+   {
    return "scissors";
+   }
 }
 
+/* prompt for user input and storing it in a var */
+var userChoice = prompt("Please choose Rock, Paper, or Scissors!");
+
+function userChoice() {
+  if (rock) {
+    if (computerChoice = rock) {
+      alert("Tie!");
+    }
+      
+    else if (computerChoice = paper) {
+      alert("Lose!");
+    }
+      
+    else (computerChoice = scissors) 
+      alert("Win!");
+    }
+    
+  else if (paper) {
+    if (computerPlay = paper) {
+      alert("Tie!");
+    }
+      
+    else if (rock) {
+      alert("Win!");
+    }
+      
+    else (scissors) 
+      alert("Lose!");
+
+  }
+    
+  else (scissors) 
+    
+    if (computerPlay = rock) {
+      alert("Lose!");
+    }
+
+    else if (computerPlay = paper) {
+      alert("Win!");
+    }
+
+    else (computerPlay= scissors) 
+      alert("Tie!");
+  
+}
